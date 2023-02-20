5f5c2eadd9d3a0c86ba75f4174b62b9bc14edfe4
nurlan1507 <baitassovnurlan1507@gmail.com>
2023-02-20 05:44:22 +0500

    have been practicing clear architecture and jetpack compose

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
@@ -8,14 +8,47 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavArgument
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgs
+import androidx.navigation.navArgument
+import com.google.android.material.shape.MarkerEdgeTreatment
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.edit_note.AddEditNoteScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.NotesScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchitectureNoteAppTheme {
-                
+                androidx.compose.material.Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.NotesScreen.route){
+                        composable(route = Screen.NotesScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route + "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(name="noteId"){ type= NavType.IntType;defaultValue = -1}, navArgument("noteColor"){type = NavType.IntType; defaultValue = -1})
+                        ){
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color ,
+                            )
+                        }
+                    }
+                }
             }
         }
     }
